ea7ce9ca6b4ba36585c6e2c2e7bb6a4dd35c0a19
Sooyeon Chae <cotndus3@naver.com>
2019-05-13 14:23:14 +0900

    #2 코드구현 - radio버튼 선택하고 해당하는 메시지박스 보여주기

--- RBtn_Msg/Form1.cs
+++ RBtn_Msg/Form1.cs
@@ -11,9 +11,31 @@ namespace RBtn_Msg
 {
     public partial class Form1 : Form
     {
+        MessageBoxButtons mbb;
+        MessageBoxIcon mbi;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (this.rbOk.Checked)
+                mbb = MessageBoxButtons.OK;     // [확인] 버튼
+            else if (this.rbOkCancel.Checked)
+                mbb = MessageBoxButtons.OKCancel;// [확인/취소] 버튼
+            else if (this.rbYesNo.Checked)
+                mbb = MessageBoxButtons.YesNo;  // [예/아니오] 버튼
+
+            if (this.rbError.Checked)
+                mbi = MessageBoxIcon.Error;         // 에러 아이콘
+            else if (this.rbInformation.Checked)
+                mbi = MessageBoxIcon.Information;   // 정보 아이콘
+            else if (this.rbQuestion.Checked)
+                mbi = MessageBoxIcon.Question;      // 질문 아이콘
+
+            MessageBox.Show("메시지 박스를 확인하세요", "알림", mbb, mbi);
+
         }
     }
 }
